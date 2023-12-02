4f080954d6a2764c4c8b344280a6b8653dde5954
williamfoschiera <william.foschiera@buser.com.br>
2023-12-01 22:35:58 -0400

    refactor project

--- 2023/day_01.py
+++ 2023/day_01.py
@@ -1,6 +1,3 @@
-from ..utils import read_files
-
-
 nums = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 int_numbers = "1 2 3 4 5 6 7 8 9".split(" ")
 translate_number = {string: number for string, number in zip(nums, int_numbers)}
@@ -21,14 +18,16 @@ def str_to_int(text, dic):
         for num in dic.keys():
             if text[i:].startswith(num):
                 resp.append(dic[num])
+                break
     return resp
 
 
-def get_digits(data):
+def get_digits(data, part_2=False):
     total = 0
     for line_str in data:
         text = line_str.strip()
-        text = str_to_int(text, translate_number)
+        if part_2:
+            text = str_to_int(text, translate_number)
 
         left = extract_digit(text)
         right = extract_digit(text[::-1])
@@ -39,5 +38,6 @@ def get_digits(data):
 
 
 if __name__ == "__main__":
-    data = read_files("input.txt")
-    extracted = get_digits(data)
+    data = open("day_01.txt", "r").readlines()
+    print(get_digits(data))
+    print(get_digits(data, True))
