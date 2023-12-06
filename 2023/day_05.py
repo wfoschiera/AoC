2b4501abfd462faf5ac7618c442abe98cbe63722
williamfoschiera <william.foschiera@buser.com.br>
2023-12-06 08:10:50 -0400

    day_05.py solução final parte 1

--- 2023/day_05.py
+++ 2023/day_05.py
@@ -1,4 +1,5 @@
 import functools
+from collections import OrderedDict
 
 test_data = """seeds: 79 14 55 13
 
@@ -35,60 +36,72 @@ humidity-to-location map:
 56 93 4"""
 
 
-def parse_input(data):
-    maps = {}
-    ordered_maps = []
-    data = data.split("\n\n")
-    for map_ in data:
-        name, values = map_.split(":")
-        if name == "seeds":
-            maps[name] = list(map(int, values.strip().split(" ")))
-            continue
-        ordered_maps.append(name)
-        maps[name] = create_map(values.strip())
-    locations = []
-    for seed in maps["seeds"]:
-        locations.append(get_location(maps, seed, ordered_maps))
-    return min(locations)
+def parse_input(data, p2=False):
+    seeds, *data = data.split("\n\n")
+    seeds = seeds.split(": ")[1]
+    seeds = [int(x) for x in seeds.split()]
+    return seeds, data
+
+def solution_p1(seeds, parsed_data):
+    for map_ in parsed_data:
+        _, *values = map_.splitlines()
+        ranges = create_map(values)
+        def get_next(seed):
+            for o, d in ranges:
+                if seed in o:
+                    return d.start - o.start + seed
+            return seed
+        seeds = [get_next(seed) for seed in seeds]
+    return min(seeds)
 
 
-def get_location(maps, index, ordered_maps):
-    if len(ordered_maps) == 1:
-        return maps[ordered_maps[0]].get(index, index)
-    index = maps[ordered_maps[0]].get(index, index)
-    location = get_location(maps, index, ordered_maps[1:])
-    return location
-
-
-def create_map(lines: str) -> dict:
-    """
-    >>> lines = '''50 98 2
-    ... 52 50 48'''
-    >>> res = create_map(lines)
-    >>> print(res)
-    >>> assert isinstance(res,dict)
-    """
-    map_ = {}
-    for line in lines.split("\n"):
-        destination, origin, range_ = line.split(" ")
-        origin = int(origin)
-        destination = int(destination)
-        range_ = int(range_)
-        origins = range(origin, origin + range_)
-        destinations = range(destination, destination + range_)
-        for orig, dest in zip(origins, destinations):
-            map_[orig] = dest
+def create_map(lines: str) -> set[tuple]:
+    map_ = set()
+    for line in lines:
+        ranges = [int(x) for x in line.split(" ")]
+        destinations = range(ranges[0], ranges[0] + ranges[2])
+        origins = range(ranges[1], ranges[1] + ranges[2])
+        map_.add((origins, destinations))
+        del ranges, destinations, origins
     return map_
+
+def solution_p2():
+    def pairs(seeds):
+        _pairs = set()
+    for i, seed in enumerate(seeds):
+        if i % 2 == 0:
+            _pairs.add((seed, seeds[i + 1]))
+    print(_pairs)
+    return _pairs
+
+
+    result = []
+    seeds_range = [range(a, a + b) for a, b in pairs(seeds)]
+    print(seeds_range)
+
 
 
 if __name__ == '__main__':
     # PART 1
     # TEST
-    # resp = parse_input(data=test_data)
-    # print(resp)
+    seeds, data = parse_input(data=test_data)
+    resp = solution_p1(seeds, data)
+    print(resp)
     # assert resp == 35
 
     # RESULT
     data = open("day_05.txt").read()
-    resp = parse_input(data=data)
+    seeds, data = parse_input(data=data)
+    resp = solution_p1(seeds, data)
     print(resp)
+
+    # PART 2
+    # # TEST
+    # resp = parse_input(data=test_data, p2=True)
+    # print(resp)
+    # assert resp == 46
+
+    # RESULT
+    # data = open("day_05.txt").read()
+    # resp = parse_input(data=data, p2=True)
+    # print(resp)
